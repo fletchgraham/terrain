72be682ede071ee24e9baadaae407066c5060f48
fletchgraham <sfletchgraham@gmail.com>
2019-12-28 13:24:25 -0500

    add a flux named tuple

--- terrain/model.py
+++ terrain/model.py
@@ -1,10 +1,15 @@
 
+from collections import namedtuple
 
 class Terrain:
     def __init__(self, size):
         self.size = size
-        self.terrain_height = empty_array(self.size)
+        self.terrain_height = array(self.size)
+        self.water_height = array(self.size)
+        self.suspended_sediment = array(self.size)
 
+Flux = namedtuple('Flux', ['left', 'right', 'top', 'bottom'])
 
-def empty_array(size):
+def array(size, value=0):
+    """Return a square, 2d array of the given value"""
     return [[0 for x in range(size)] for y in range(size)]
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,9 +1,27 @@
-from terrain.model import Terrain
+from terrain.model import Terrain, Flux
 
 def test_init_terrain():
-    my_terrain = Terrain(1024)
-    assert my_terrain.size == 1024
+
+    SIZE = 256
+
+    my_terrain = Terrain(SIZE)
+    assert my_terrain.size == SIZE
 
     # test that terrain height is a 2d array
-    assert len(my_terrain.terrain_height) == 1024
-    assert len(my_terrain.terrain_height[0]) == 1024
+    assert len(my_terrain.terrain_height) == SIZE
+    assert len(my_terrain.terrain_height[0]) == SIZE
+
+    # test that water height is a 2d array
+    assert len(my_terrain.water_height) == SIZE
+    assert len(my_terrain.water_height[0]) == SIZE
+
+def test_flux():
+
+    SIZE = 256
+
+    flux = Flux(.1, .2, .3, .4)
+
+    assert flux.left == .1
+    assert flux.right == .2
+    assert flux.top == .3
+    assert flux.bottom == .4
